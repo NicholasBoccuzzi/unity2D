c0245cd939a371efe32a403adc7b88bbaae167f4
Nicholas Boccuzzi <Nicholas.R.Boccuzzi@gmail.com>
2018-05-22 23:32:31 -0400

    first commit

---
+++ HelloWorld/Assets/Assets/helloWorld.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class helloWorld : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		print("Hello Nicky B");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
