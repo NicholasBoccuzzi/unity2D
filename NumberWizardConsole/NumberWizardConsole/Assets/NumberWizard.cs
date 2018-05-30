3a206a1db43dd77f30ff75a29449b216e57982de
Nicholas Boccuzzi <Nicholas.R.Boccuzzi@gmail.com>
2018-05-30 14:14:35 -0400

    learning to handle key presses

--- NumberWizardConsole/NumberWizardConsole/Assets/NumberWizard.cs
+++ NumberWizardConsole/NumberWizardConsole/Assets/NumberWizard.cs
@@ -6,14 +6,31 @@ public class NumberWizard : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
+		int max = 1000;
+		int min = 1;
+
 		Debug.Log("Welcome to Number Wizard!");
-		Debug.Log("Pick a number");
-		Debug.Log("Highest number is: ");
-		Debug.Log("Lowest number is: ");
+		Debug.Log("Pick a number, but don't tell me what it is!");
+		Debug.Log("Highest number is: " + max);
+		Debug.Log("Lowest number is: " + min);
+		Debug.Log("Tell me if your number is higher or lower than" + (max/min));
+		Debug.Log("Push Up = Higher, Push Down = Lower, Push Enter = Correct");
 	}
 	
 	// Update is called once per frame
 	void Update () {
+		if (Input.GetKeyDown(KeyCode.UpArrow)) {
+			Debug.Log("You Win!");
 		
+		}
+
+		if (Input.GetKeyDown(KeyCode.DownArrow)) {
+			Debug.Log("You Win!");
+
+		}
+
+		if (Input.GetKeyDown(KeyCode.Return)) {
+			Debug.Log("You Win!");
+		}
 	}
 }
