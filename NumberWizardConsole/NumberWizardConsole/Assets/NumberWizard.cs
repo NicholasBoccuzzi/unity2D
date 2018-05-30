b61f292efe0265068becd27cb29d86ce895ba337
Nicholas Boccuzzi <Nicholas.R.Boccuzzi@gmail.com>
2018-05-30 15:51:10 -0400

    complete unity console game

--- NumberWizardConsole/NumberWizardConsole/Assets/NumberWizard.cs
+++ NumberWizardConsole/NumberWizardConsole/Assets/NumberWizard.cs
@@ -3,9 +3,9 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class NumberWizard : MonoBehaviour {
-		int max = 1000;
-		int min = 1;
-		int guess = 500;
+		int max;
+		int min;
+		int guess;
 	// Use this for initialization
 	void Start () {
 		StartGame();
@@ -18,26 +18,34 @@ public class NumberWizard : MonoBehaviour {
 			Debug.Log("So your number is higher than " + guess + "? Hmm, let me think...");
 			min = guess;
 			guess = (max + min)/2;
-			Debug.Log("Is your number, " + guess + "?");
-			
+			NextGuess();
 		} else if (Input.GetKeyDown(KeyCode.DownArrow)) {
 			Debug.Log("So your number is lower than " + guess + "? Hmm, let me think...");
 			max = guess;
-			guess = (max + min)/2;
-			Debug.Log("Is your number, " + guess + "?");
-	
+			guess = (max + min)/2;	
+			NextGuess();			
 		} else if (Input.GetKeyDown(KeyCode.Return)) {
 			Debug.Log("I did it! Your number is " + guess + "!");
+			Debug.Log("Let's play again!");
+			StartGame();
 		}
 	}
 
 	void StartGame () {
-		max = max + 1;
+		max = 1000;
+		min = 1;
+		guess = 500;
+
 		Debug.Log("Welcome to Number Wizard!");
 		Debug.Log("Pick a number, but don't tell me what it is!");
 		Debug.Log("Highest number is: " + max);
+		max = max + 1;
 		Debug.Log("Lowest number is: " + min);
 		Debug.Log("Tell me if your number is higher or lower than " + guess);
 		Debug.Log("Push Up = Higher, Push Down = Lower, Push Enter = Correct");
+	}
+
+	void NextGuess () {
+			Debug.Log("Is your number, " + guess + "?");		
 	}
 }
