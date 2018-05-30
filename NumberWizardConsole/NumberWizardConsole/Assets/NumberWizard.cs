e9cadfb22f1ffc486ffd428ba6cbe0a6a61662f6
Nicholas Boccuzzi <Nicholas.R.Boccuzzi@gmail.com>
2018-05-30 11:15:35 -0400

    learning to print with C#

--- NumberWizardConsole/NumberWizardConsole/Assets/NumberWizard.cs
+++ NumberWizardConsole/NumberWizardConsole/Assets/NumberWizard.cs
@@ -2,11 +2,14 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Wizard : MonoBehaviour {
+public class NumberWizard : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-		
+		Debug.Log("Welcome to Number Wizard!");
+		Debug.Log("Pick a number");
+		Debug.Log("Highest number is: ");
+		Debug.Log("Lowest number is: ");
 	}
 	
 	// Update is called once per frame
