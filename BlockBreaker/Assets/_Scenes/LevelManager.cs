5cf3ca02b0c616b339202eb05c252d4d27e194d9
Nicholas Boccuzzi <Nicholas.R.Boccuzzi@gmail.com>
2018-06-05 17:01:59 -0400

    block breaker

---
+++ BlockBreaker/Assets/_Scenes/LevelManager.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelManager : MonoBehaviour {
+
+	public void LoadLevel(string name) {
+		Debug.Log("Level Load Requested for: " + name);
+		Application.LoadLevel(name);
+	}
+
+	public void QuitLevel(string name) {
+		Debug.Log("Quiting the level: " + name);
+		Application.Quit();
+	}
+}
