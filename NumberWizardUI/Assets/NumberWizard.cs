0284918e74871f177c4de0a902eb9babc76a58af
Nicholas Boccuzzi <Nicholas.R.Boccuzzi@gmail.com>
2018-06-05 12:48:08 -0400

    editing NumberWizardCode

---
+++ NumberWizardUI/Assets/NumberWizard.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class NumberWizard : MonoBehaviour {
+		int max;
+		int min;
+		int guess;
+		public Text text;
+		int maxGuesses = 5;
+
+	// Use this for initialization
+	void Start () {
+		StartGame();
+	}
+
+	public void GuessHigher() {
+			min = guess;
+			NextGuess();
+	}
+
+	public void GuessLower() {
+			max = guess;
+			NextGuess();	
+	}
+
+	void StartGame () {
+		max = 1000;
+		min = 1;
+		guess = 500;
+
+		max = max + 1;
+	}
+
+	void NextGuess () {
+		guess = (max + min)/2;
+		maxGuesses -= 1;
+		text.text = ("Is your number: " + guess + "?");
+		if (maxGuesses <= 0) {
+			Application.LoadLevel("Win");
+		}
+	}
+}
