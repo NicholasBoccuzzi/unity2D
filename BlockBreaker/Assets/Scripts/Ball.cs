5ec4a06a683824c3557b95cfbe25c62eace05019
Nicholas Boccuzzi <Nicholas.R.Boccuzzi@gmail.com>
2018-06-06 16:23:24 -0400

    ball starts on top of current position of paddle relatively speaking

---
+++ BlockBreaker/Assets/Scripts/Ball.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour {
+
+	public PaddleMovement paddle;
+
+	private bool started = false;
+	private Rigidbody2D rigi;
+	private void Awake() {
+		rigi = GetComponent<Rigidbody2D>();
+	}
+
+	private Vector3 paddleToBallVector;
+	// Use this for initialization
+	void Start () {
+		paddleToBallVector = this.transform.position - paddle.transform.position;
+		print (paddleToBallVector);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (!started) {
+			this.transform.position = paddle.transform.position + paddleToBallVector;
+
+			if (Input.GetMouseButtonDown(0)){
+				started = true;
+				rigi.velocity = new Vector2 (0f, 14f);
+			}
+
+		}
+	}
+}
---
+++ BlockBreaker/Assets/Scripts/PaddleMovement.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PaddleMovement : MonoBehaviour {
+
+	float xValue;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Vector3 paddlePos = new Vector3 ( 8, this.transform.position.y, 0f);
+		xValue = Mathf.Clamp(Input.mousePosition.x/Screen.width * 16, .5f, 15.5f);
+
+		paddlePos.x = xValue;
+
+		this.transform.position = paddlePos;
+	}
+}
