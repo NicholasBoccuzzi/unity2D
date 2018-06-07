56a55ce3d8d393c113f058d20b8e6cf5e57e36df
Nicholas Boccuzzi <Nicholas.R.Boccuzzi@gmail.com>
2018-06-07 11:52:11 -0400

    can successfully alternate between the different tones for each health level

--- BlockBreaker/Assets/Scripts/Brick.cs
+++ BlockBreaker/Assets/Scripts/Brick.cs
@@ -4,18 +4,41 @@ using UnityEngine;
 
 public class Brick : MonoBehaviour {
 	public int health;
+	private Renderer rend;
+	private SpriteRenderer spriteRenderer;
+	public Sprite[] brickSprites;
+	public AudioClip[]  audioarray;
+	public AudioSource audioSource;
+
 	// Use this for initialization
 	void Start () {
+		rend = GetComponent<Renderer>();
+		spriteRenderer = gameObject.GetComponent<SpriteRenderer>();
 	}
 	
 	void OnCollisionEnter2D(Collision2D collision) {
-		health -= 1; 
-		GetComponent<AudioSource> ().Play();
+		if (health == 3) {
+			audioSource.PlayOneShot(audioarray[2]);
+		} else if (health == 2) {
+			audioSource.PlayOneShot(audioarray[1]);			
+		} else if (health == 1) {
+			audioSource.PlayOneShot(audioarray[0]);
+		}
+		health -= 1;
+
 	}
 
 	void Update () {
-		if (health <= 0) {
-			Object.Destroy(gameObject);
+		Debug.Log(brickSprites);
+		if (health == 3) {
+			spriteRenderer.sprite = brickSprites[2];
+		} else if (health == 2) {
+			spriteRenderer.sprite = brickSprites[1];
+		} else if (health == 1) {
+			spriteRenderer.sprite = brickSprites[0];
+		} else if (health <= 0) {
+			rend.enabled = false;
+			Destroy(gameObject);
 		}
 	}
 }
